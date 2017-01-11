5df2981eaf1246e6fec2b5c2313d18ccd7d6e160
adenilson <adenilson.silva@vistajet.com>
2017-01-11 17:52:37 -0200

    Interceptor was added.

--- app/src/main/java/com/treinamento/adenilson/myretrofitapplication/MainActivity.java
+++ app/src/main/java/com/treinamento/adenilson/myretrofitapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.treinamento.adenilson.myretrofitapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +17,18 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.treinamento.adenilson.myretrofitapplication.domain.GitHubStatusApi;
+import com.treinamento.adenilson.myretrofitapplication.domain.GitHubUserApi;
 import com.treinamento.adenilson.myretrofitapplication.domain.entity.Status;
+import com.treinamento.adenilson.myretrofitapplication.domain.entity.User;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -32,9 +41,11 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mImageViewGit;
     private TextView mTextViewStatus;
     private GitHubStatusApi mStatusApi;
-    private TextInputEditText mTextInputUsername;
-    private TextInputEditText mTextInputPassword;
+    private GitHubUserApi mUserApi;
+    private TextInputLayout mTextInputUsername;
+    private TextInputLayout mTextInputPassword;
     private Button mButtonLogin;
+    private Button mButtonAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,8 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void requestStatusApi() {
 
-
-
         Call<Status> call = mStatusApi.lastMessage();
         call.enqueue(new Callback<Status>() {
             @Override
@@ -90,41 +99,128 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<Status> call, Throwable t) {
-                changeStatus(Status.Type.MAJOR.getColor(), t.getMessage());
             }
         });
     }
 
     private void changeStatus(int color, String message) {
-        DrawableCompat.setTint(mImageViewGit.getDrawable(), color);
         mTextViewStatus.setText(message);
         mTextViewStatus.setTextColor(color);
-
     }
 
     private void bindViews() {
         mImageViewGit = (ImageView) findViewById(R.id.image_view_git);
         mTextViewStatus = (TextView) findViewById(R.id.text_view_status);
-        mTextInputUsername = (TextInputEditText) findViewById(R.id.text_input_user);
-        mTextInputPassword = (TextInputEditText) findViewById(R.id.text_input_pass);
+
+        TextWatcher textWatcher = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mTextInputUsername.setError(null);
+            }
+        };
+
+        TextWatcher textWatcher1 = new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mTextInputPassword.setError(null);
+            }
+        };
+        mTextInputUsername = (TextInputLayout) findViewById(R.id.text_input_user);
+        mTextInputPassword = (TextInputLayout) findViewById(R.id.text_input_pass);
+        mTextInputUsername.getEditText().addTextChangedListener(textWatcher);
+        mTextInputPassword.getEditText().addTextChangedListener(textWatcher1);
+
+        mButtonAuth = (Button) findViewById(R.id.button_auth);
 
         mButtonLogin = (Button) findViewById(R.id.button_login);
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = mTextInputUsername.getText().toString();
-                String password = mTextInputPassword.getText().toString();
+                String username = mTextInputUsername.getEditText().getText().toString();
+                String password = mTextInputPassword.getEditText().getText().toString();
                 if(!username.isEmpty()
                         || !password.isEmpty()){
-
+                    requestLogin(username, password);
 
                     
                 }else{
                     Toast.makeText(MainActivity.this, R.string.message_empty_field, Toast.LENGTH_SHORT)
                             .show();
+                    mTextInputUsername.setError("Campo vazio");
+                    mTextInputPassword.setError("Campo vazio");
                 }
             }
         });
+    }
+
+    private void requestLogin(final String username, final String password) {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+                Request request = original.newBuilder()
+                        .header("Authorization", Credentials.basic(username, password))
+                        .method(original.method(), original.body())
+                        .build();
+                return chain.proceed(request);
+            }
+        });
+
+        OkHttpClient client = httpClient.build();
+
+
+        final Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .baseUrl(GitHubUserApi.BASE_URL)
+                .build();
+
+        mUserApi = retrofit.create(GitHubUserApi.class);
+
+        mUserApi.getUser().enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if(response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, response.body().login, Toast.LENGTH_SHORT).show();
+                }else{
+                    try {
+                        Toast.makeText(MainActivity.this, response.errorBody().string(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 
